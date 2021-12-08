8277c366c7f3501f646d4d0f4cf9189b95a8b080
Ayman Hasib <77047245+AymanHasib@users.noreply.github.com>
2021-12-08 12:59:23 +0600

    Class work 1 added

---
+++ Lab Class Work/Lab_1/Lab_1.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT); //[pin no., state - INPUT/OUTPUT]
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);  //[pin no. , value = HIGH/1/(5v) OR LOW/0/(0v)]
+
+}
